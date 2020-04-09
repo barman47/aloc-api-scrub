9a79e905a08e834446e3ed7fc39b070c3f3389fb
Uzoanya Dominic <nomsouzoanya@yahoo.co.uk>
2020-04-09 19:25:11 +0100

    Fixed Connection Closing Issue

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-const axios = require('axios');
+const getJSON = require('get-json');
 
 const questions = [
     'english', 
@@ -21,44 +21,81 @@ const questions = [
     'history'
 ];
 
+
 let questionsArray = [];
 
-questions.forEach(item => {
-    for (let i = 0; i < 999; i++) {
-        axios.get(`https://questions.aloc.ng/api/q/100?subject=${item}`)
-        .then(res => {
-            const { data } = res;
-            data.data.forEach(item => {
-                const question = {
-                    subject: data.subject,
-                    "headers": [
-                        {"text": ""},
-                        {"image": ""}
-                    ],
-                    year: item.examyear,
-                    type: item.examtype.toUpperCase(),
-                    number: item.id,
-                    question: item.question,
-                    options: {
-                        A: item.option.a,
-                        B: item.option.b,
-                        C: item.option.c,
-                        D: item.option.d
-                    },
-                    answer: item.answer,
-                    solution: item.solution,
-                    image: item.image
-                };
-                questionsArray.push(question);
+const getData = () => {
+    setTimeout(() => console.log('Starting Transfer . . .'), 2000);
+    questions.forEach(questionItem => {
+        for (let i = 0; i < 9; i++) {
+            getJSON(`https://questions.aloc.ng/api/q/40?subject=${questionItem}`, (err, res) => {
+                if (err) {
+                    return console.log(err);
+                }
+                res.data.forEach(item => {
+                    const question = {
+                        subject: res.subject,
+                        "headers": [
+                            {"text": ""},
+                            {"image": ""}
+                        ],
+                        year: item.examyear,
+                        type: item.examtype.toUpperCase(),
+                        number: item.id,
+                        question: item.question,
+                        options: {
+                            A: item.option.a,
+                            B: item.option.b,
+                            C: item.option.c,
+                            D: item.option.d
+                        },
+                        answer: item.answer,
+                        solution: item.solution,
+                        image: item.image
+                    };
+                    questionsArray.push(question);
+                    fs.appendFile(`${questionItem}.json`, JSON.stringify(questionsArray), (err) => {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        fs.closeSync(1);
+                        console.log('Saving questions');
+                        questionsArray = [];
+                    });
+                });
             });
-        })
-        .catch(err => console.error(err));
-    }
-    fs.appendFileSync(`${item}.json`, JSON.stringify(questionsArray), (err) => {
-        if (err) {
-            return console.error(err);
         }
-        console.log('file written');
     });
-    questionsArray = [];
-});
+}
+
+getData();
+
+// const sortQuestions = () => {
+//     let sortedQuestions = [];
+//     questions.forEach(question => {
+//         fs.readFile(`${question}.json`, 'utf8', (err, data) => {
+//             if (err) {
+//                 return console.error(err);
+//             }
+//             sortedQuestions = [...data];
+//             sortedQuestions = sortedQuestions.filter((item, index) => data.indexOf(item) !== index);
+//             console.log(sortQuestions);
+//             fs.writeFileSync(`${question}.json`, JSON.parse(sortedQuestions), (err) => {
+//                 if (err) {
+//                     return console.error(err)
+//                 }
+//                 console.log(`Removed ${question} duplicates`);
+//             });
+//         });
+//     });
+// }
+
+// sortQuestions();
+
+// function startTransfer (callback) {
+//     console.log('Starting download. . . . . . .');
+//     getData();
+//     if (typeof callback === 'function') {
+//         callback();
+//     }
+// }
